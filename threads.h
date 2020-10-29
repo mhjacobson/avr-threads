7a467f3ddd3b27288bcb5dc156b76f6461123198
Matt Jacobson <mhjacobson@me.com>
2020-10-29 02:05:12 -0700

    threads.h: add header guards

--- threads.h
+++ threads.h
@@ -6,8 +6,13 @@
 
 #include <stddef.h>
 
+#ifndef THREADS_H
+#define THREADS_H
+
 typedef struct thread *thread_t;
 
 void thread_init(void);
 thread_t thread_create(void (*start)(void));
 void thread_switch(void);
+
+#endif /* THREADS_H */
