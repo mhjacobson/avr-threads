69fd518276a633867cda04b757cd54b292a0bc52
Matt Jacobson <mhjacobson@me.com>
2020-10-29 00:17:14 -0700

    threads.c: eliminate extra members in `struct avr_state`, which also broke swapping

--- threads.c
+++ threads.c
@@ -11,7 +11,7 @@
 struct avr_state {
 	uint8_t  r2,  r3,  r4,  r5,  r6,  r7,  r8,  r9, 
 	        r10, r11, r12, r13, r14, r15, r16, r17, 
-	        r28, r29, yl, yh;
+	         yl, yh;
 	uint8_t sreg;
 	uint8_t pcl;
 	uint8_t pch;
